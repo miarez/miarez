8597c9f3db78a6209c1c8062c222a7be89330eea
stas <pakhomovs@hotmail.com>
2024-05-21 14:57:32 -0400

    first commit

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 ##  My GitHub activity stats
 
-<!-- Found out I could do this from @rodrigogiraoserrao! -->
-
-<img src="https://github.com/miarez/miarez/blob/main/general_stats.svg" alt="GitHub Statistics" loading="lazy">
-
-<img src="language_stats.svg" alt="Top Languages" loading="lazy">
+<img src="general_stats.svg" alt="General Statistics" loading="lazy">
+<img src="language_stats.svg" alt="Language Statistics" loading="lazy">
 
 <p align='center'><img src='https://visitor-badge.laobi.icu/badge?page_id=miarez'></p>
+
 
--- fetch-github-stats.js
+++ fetch-github-stats.js
@@ -1,22 +1,40 @@
 const https = require('https');
 const fs = require('fs');
-const url = 'https://github-readme-stats.vercel.app/api?username=miarez';
 
-https.get(url, (response) => {
-  let data = '';
+run(
+  'https://github-readme-stats.vercel.app/api?username=miarez&hide=prs,issues,contribs&show_icons=true&theme=dark&rank_icon=percentile',
+  'general_stats.svg'
+)
 
-  // A chunk of data has been received.
-  response.on('data', (chunk) => {
-    data += chunk;
-  });
+run(
+  'https://github-readme-stats.vercel.app/api/top-langs/?username=miarez&theme=dark&layout=donut-vertical&langs_count=10',
+  'language_stats.svg'
+)
 
-  // The whole response has been received.
-  response.on('end', () => {
-    fs.writeFile('general_stats.svg', data, (err) => {
-      if (err) throw err;
-      console.log('The file has been saved!');
+
+
+function run(
+  url,
+  output
+){
+  https.get(url, (response) => {
+    let data = '';
+
+    // A chunk of data has been received.
+    response.on('data', (chunk) => {
+      data += chunk;
     });
+
+    // The whole response has been received.
+    response.on('end', () => {
+      fs.writeFile(output, data, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      });
+    });
+  }).on('error', (err) => {
+    console.error('Error fetching the URL:', err.message);
   });
-}).on('error', (err) => {
-  console.error('Error fetching the URL:', err.message);
-});
+}
+
+
